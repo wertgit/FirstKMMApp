90c3ca272b96a87831639117039851b4ed95800b
Caeser <caeser.bakabulindi@myfave.com>
2022-10-19 02:36:38 +0800

    add some ios framework stuff to make poject prun on iOS

--- shared/src/iosSimulatorArm64Main/kotlin/com/bakabool/firstkmmapp/shared/cache/DatabaseDriverFactory.kt
+++ shared/src/iosSimulatorArm64Main/kotlin/com/bakabool/firstkmmapp/shared/cache/DatabaseDriverFactory.kt
@@ -1,9 +1,14 @@
 package com.bakabool.firstkmmapp.shared.cache
 
+import com.squareup.sqldelight.db.SqlDriver
+
 /**
  * The {expect} keyword tells the compiler to look for an actual implementation of this class
  * in the platform-specific sourceset (androidMain and iosMain)
  * in the same package (commonMain/com/bakabool/firstkmmapp/shared/cache) in all sourcesets.
  */
 actual class DatabaseDriverFactory {
+    actual fun createDriver(): SqlDriver {
+        TODO("Not yet implemented")
+    }
 }
